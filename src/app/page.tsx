56c8ce1b4c5d5345f7923de95ac6e4432b0b6459
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-12-14 17:23:56 +0000

    above FAQ section, i want pricing table, use this code : import { Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { HeroHeader } from '@/components/layout/HeroHeader';
 import HeroSection from '@/components/layout/hero-section';
 import { BentoGridThirdDemo } from '@/components/layout/feature-section';
 import { FaqSection } from '@/components/layout/faq-section';
+import PricingSection from '@/components/layout/pricing-section';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <div className="py-20">
         <BentoGridThirdDemo />
       </div>
+      <PricingSection />
       <FaqSection />
     </div>
   );
