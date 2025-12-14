81ed1a6dee0836652bfc9045ed85a7230ad4e175
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-12-14 06:27:45 +0000

    there is a logo already on sidebar, i dont want one here, remove it  (_f

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,6 @@
 'use client';
 
 import AuthButton from '@/components/auth-button';
-import Logo from '@/components/logo';
 import { ThemeToggle } from '@/components/theme-toggle';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 
@@ -12,7 +11,6 @@ export default function Header() {
       <div className="container flex h-14 items-center">
         <div className="mr-4 flex items-center gap-2">
           <SidebarTrigger className="md:hidden" />
-          <Logo />
         </div>
         <div className="flex flex-1 items-center justify-end space-x-2">
           <ThemeToggle />
