0e26da3fd8e1e5e89ae7bf515f6eaf65bc2acbbb
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-12-14 17:47:23 +0000

    i dont wanr Microsoft button, make Google button bigger (_for element <B

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -99,7 +99,7 @@ export default function LoginPage() {
                     </TabsList>
                     <TabsContent value="signin">
                         <div className="bg-card rounded-b-lg border p-6 shadow-md">
-                            <div className="mt-6 grid grid-cols-2 gap-3">
+                            <div className="mt-6 grid grid-cols-1 gap-3">
                                 <Button type="button" variant="outline" onClick={handleAuthAction}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="0.98em" height="1em" viewBox="0 0 256 262">
                                         <path fill="#4285f4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path>
@@ -108,15 +108,6 @@ export default function LoginPage() {
                                         <path fill="#eb4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"></path>
                                     </svg>
                                     <span>Google</span>
-                                </Button>
-                                <Button type="button" variant="outline" onClick={handleAuthAction}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256">
-                                        <path fill="#f1511b" d="M121.666 121.666H0V0h121.666z"></path>
-                                        <path fill="#80cc28" d="M256 121.666H134.335V0H256z"></path>
-                                        <path fill="#00adef" d="M121.663 256.002H0V134.336h121.663z"></path>
-                                        <path fill="#fbbc09" d="M256 256.002H134.335V134.336H256z"></path>
-                                    </svg>
-                                    <span>Microsoft</span>
                                 </Button>
                             </div>
                             <hr className="my-4 border-dashed" />
@@ -125,7 +116,7 @@ export default function LoginPage() {
                     </TabsContent>
                      <TabsContent value="signup">
                         <div className="bg-card rounded-b-lg border p-6 shadow-md">
-                           <div className="mt-6 grid grid-cols-2 gap-3">
+                           <div className="mt-6 grid grid-cols-1 gap-3">
                                 <Button type="button" variant="outline" onClick={handleAuthAction}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="0.98em" height="1em" viewBox="0 0 256 262">
                                         <path fill="#4285f4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path>
@@ -134,15 +125,6 @@ export default function LoginPage() {
                                         <path fill="#eb4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"></path>
                                     </svg>
                                     <span>Google</span>
-                                </Button>
-                                <Button type="button" variant="outline" onClick={handleAuthAction}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256">
-                                        <path fill="#f1511b" d="M121.666 121.666H0V0h121.666z"></path>
-                                        <path fill="#80cc28" d="M256 121.666H134.335V0H256z"></path>
-                                        <path fill="#00adef" d="M121.663 256.002H0V134.336h121.663z"></path>
-                                        <path fill="#fbbc09" d="M256 256.002H134.335V134.336H256z"></path>
-                                    </svg>
-                                    <span>Microsoft</span>
                                 </Button>
                             </div>
                             <hr className="my-4 border-dashed" />
@@ -154,5 +136,6 @@ export default function LoginPage() {
         </section>
     );
 }
+
 
 
