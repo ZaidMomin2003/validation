364133eb0c2f6053f40d482167fa562679fc9c61
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-12-22 04:42:38 +0000

    i want to eliminate that arrow at last (_for element div_)

--- src/components/layout/HeroHeader.tsx
+++ src/components/layout/HeroHeader.tsx
@@ -30,7 +30,6 @@ export const HeroHeader = () => {
                         <PartyPopper className="mr-2 h-4 w-4" />
                         <span className="font-semibold md:hidden">Lifetime Deal : 69$ for 500k credits yearly</span>
                         <span className="hidden font-semibold md:inline">Lifetime Deal : 69$ for 500k credits yearly</span>
-                        <ArrowRight className="ml-2 h-4 w-4 transform transition-transform duration-300 group-hover:translate-x-1" />
                     </div>
                 </Link>
             </div>
