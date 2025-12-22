9bb4117cbc57168bc67ff0bdfaaae1958f97485e
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-12-22 04:19:40 +0000

    this line, i want to say Wisdomis.fun and talxify.space use our fast and

--- src/components/layout/link-preview-demo.tsx
+++ src/components/layout/link-preview-demo.tsx
@@ -6,14 +6,14 @@ export function LinkPreviewDemo() {
   return (
     <div className="flex justify-center items-center h-[20rem] flex-col px-4">
       <div className="text-neutral-500 dark:text-neutral-400 text-xl md:text-3xl max-w-3xl mx-auto mb-10">
-        <LinkPreview url="https://nextjs.org" className="font-bold">
-          Next.js
+        <LinkPreview url="https://wisdomis.fun" className="font-bold">
+          Wisdomis.fun
         </LinkPreview>{" "}
         and{" "}
-        <LinkPreview url="https://firebase.google.com" className="font-bold">
-          Firebase
+        <LinkPreview url="https://talxify.space" className="font-bold">
+          talxify.space
         </LinkPreview>{" "}
-        power our fast and reliable platform.
+        use our fast and reliable platform.
       </div>
       <div className="text-neutral-500 dark:text-neutral-400 text-xl md:text-3xl max-w-3xl mx-auto">
         Styled with{" "}
