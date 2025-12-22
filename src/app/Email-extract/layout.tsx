a78530a3f26e0a412a557e4e6478fa79174b96e1
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-12-22 13:56:10 +0000

    i want to create new non auth page called /Email-extract, it should do t

---
+++ src/app/Email-extract/layout.tsx
@@ -0,0 +1,15 @@
+
+import Footer from '@/components/layout/footer';
+import { HeroHeader } from '@/components/layout/HeroHeader';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-neutral-950 text-white">
+        <HeroHeader />
+        <main className='pt-20'>
+            {children}
+        </main>
+        <Footer />
+    </div>
+  )
+}
